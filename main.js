ead42d8f31e947c7aff594e29fefb6ca53995d27
Eletrodude <leonardofaleiro81@gmail.com>
2024-11-25 16:02:14 +0100

    Added js and h1

---
+++ main.js
@@ -0,0 +1,3 @@
+
+document.getElementById("Home").style.color = "red"
+document.getElementById("Home").textContent = "De reden dat je gebruikt een ID een keer, is want het is precies en stopt gelijk waneer je heb het gevonden" 
